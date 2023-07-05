2f38b02022f0e2e432cd896c3b2d381d15f12197
Younganswer <dudtj1999@gmail.com>
2023-07-05 14:28:28 +0900

    .

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -8,4 +8,10 @@ sed -i "5i\ \   server_name www.$DOMAIN_NAME $DOMAIN_NAME;" /etc/nginx/sites-ava
 sed -i "7d" /etc/nginx/sites-available/default
 sed -i "7i\ \   ssl_certificate $CERTS_;" /etc/nginx/sites-available/default
 
+#sed -i '40s/.*/\tlog_format debug '\''$remote_addr - $remote_user [$time_local] '\'' '\''"$request" $status $body_bytes_sent '\'' '\''"$http_referer" "$http_user_agent"'\'';/' /etc/nginx/nginx.conf
+#sed -i '41s/.*/\taccess_log \/var\/log\/nginx\/access.log debug;/' /etc/nginx/nginx.conf
+#sed -i '42s/.*/\terror_log \/var\/log\/nginx\/error.log debug;/' /etc/nginx/nginx.conf
+
+cat /etc/nginx/nginx.conf
+
 nginx -g "daemon off;"
